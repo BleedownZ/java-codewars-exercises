136ee9893fd6447d46d34e5d834bdcb143ddc775
BleedownZ <101429170+BleedownZ@users.noreply.github.com>
2025-07-28 12:07:54 +0200

    Update Find_Stray_Number.java

--- _7kyu/Find_stray_number/Find_Stray_Number.java
+++ _7kyu/Find_stray_number/Find_Stray_Number.java
@@ -1,26 +1,34 @@
 package _7kyu.Find_stray_number;
 import java.util.Arrays;
-
-public class Find_Stray_Number {
+public class Find_Stray_Number_Ref {
 
 	public static void main(String[] args) {
-		int[] numbers1 = {17, 99, 99, 99, 99};
-		int[] numbers2 = {7, 7, 7, 5, 7};
-		int[] numbers3 = {99, 3, 3, 3, 3};
-		int[] numbers4 = {0, 0, 0, 4, 0};
-	 System.out.println(stray(numbers1));//17
-	 System.out.println(stray(numbers2));//5
-	 System.out.println(stray(numbers3));//99
-	 System.out.println(stray(numbers4));//4
-
+		int[] numbers1 = {100, 100, 200, 100};
+		int[] numbers2 = {42, 42, 42, 42, 42, 17, 42, 42, 42};
+		int[] numbers3 = {-1, -1, -1, -5, -1};
+		int[] numbers4 = {6, 1, 6, 6, 6, 6};
+		
+		System.out.println(stray(numbers1));//200
+		System.out.println(stray(numbers2));//17
+		System.out.println(stray(numbers3));//-5
+		System.out.println(stray(numbers4));//1
 	}
-	static int stray(int[] numbers) {
-	    //This sorts the array in ascending order.
-	    Arrays.sort(numbers);
-	    /*This is a ternary operator
-	     * If the first number is different from the second, then the stray number is the first one.
-	     * Otherwise, the stray number must be at the end (last index), because all the rest are the same.
-	     */
-	    return numbers[0] != numbers[1] ? numbers[0] : numbers[numbers.length - 1];
-	  }
+	 static int stray(int[] numbers) {
+		 
+		 		//The arrays are valid 
+		    if(numbers.length >= 3){
+		    	//This sorts the array in ascending order.
+		    	Arrays.sort(numbers);
+			    /*This is a ternary operator
+			     * If the first number is different from the second, then the stray number is the first one.
+			     * Otherwise, the stray number must be at the end (last index), because all the rest are the same.
+			     */
+
+		    	return numbers[0] != numbers[1] ? numbers[0] : numbers[numbers.length - 1];
+		    
+		    }else{
+		      System.out.println("The input no be valid");
+		    }
+		    return -1;
+		  }
 }
