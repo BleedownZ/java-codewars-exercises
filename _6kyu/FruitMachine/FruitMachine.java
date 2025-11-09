6b85a3112855e9453c7d8ca7353a0bd52ef36dfd
Bleedownz <g.6578@live.com>
2025-11-09 18:04:12 +0100

    kata next

---
+++ _6kyu/FruitMachine/FruitMachine.java
@@ -0,0 +1,131 @@
+package _6kyu.FruitMachine;
+
+public class FruitMachine {
+    public static void main(String[] args) {
+        String[][] reels = {
+                {"Wild","Star","Bell","Shell","Seven","Cherry","Bar","King","Queen","Jack"},
+                {"Wild","Star","Bell","Shell","Seven","Cherry","Bar","King","Queen","Jack"},
+                {"Wild","Star","Bell","Shell","Seven","Cherry","Bar","King","Queen","Jack"}
+        };
+
+        int[] spin = {5, 5, 5};
+        int[] spin1 = {3, 3, 3};
+        int[] spin2 = {4, 4, 4};
+        System.out.println(fruit(reels,spin));
+        System.out.println(fruit(reels,spin1));
+        System.out.println(fruit(reels,spin2));
+
+    }
+    public static int fruit(final String[][] reels, final int[] spins) {
+        int items = 0;
+        int score = 0;
+        for (String [] count: reels){
+            items += count.length;
+        }
+        if (reels.length == 3 && items == 30 && spins.length == 3){
+            //Created String array only with the spins
+            String [] fruits= new String[3];
+            int wild = 0;
+            int fruit2 = 0;
+            int fruit3 = 0;
+            //To get the Fruits of spins
+            for (int i = 0; i < spins.length ; i++){
+                int number = spins[i];
+
+                String fruit = reels[i][number];
+                switch (fruit){
+                    case "Wild":
+                        fruits[i] = fruit;
+                        wild++;
+                        break;
+                    case "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack":
+                        fruits[i] = fruit;
+                        break;
+                    default:
+                        return 0;
+                }
+            }
+            //There are two, three or two more wild (fruits)
+            int noEqual = 0;
+            String mostRepeat = "";
+            for (int h = 0; h < fruits.length; h++){
+                for (int g = 0; g < fruits.length; g++){
+
+                    if (h != g) {
+
+                        if (fruits[h].equals(fruits[g])) {
+                            fruit2 = 1;
+                            mostRepeat = fruits[h];
+                        }else {
+                            noEqual = 1;
+                        }
+                        if (fruit2 == 1 && fruit3 == 0 && noEqual == 0 && g == fruits.length-1) {
+                            fruit3 = 1;
+                            mostRepeat = fruits[g];
+                        }
+                    }
+                }
+            }
+
+            boolean threeSame = fruit3 == 1 && noEqual == 0;
+            boolean twoMoreWild = fruit2 == 1 && wild == 1;
+            boolean twoSame = fruit2 == 1 && noEqual == 1;
+            //Put score to fruits
+            int baseScore = 0;
+            switch (mostRepeat) {
+                case "Wild":
+                    baseScore = 10;
+                    break;
+                case "Star":
+                    baseScore = 9;
+                    break;
+                case "Bell":
+                    baseScore = 8;
+                    break;
+                case "Shell":
+                    baseScore = 7;
+                    break;
+                case "Seven":
+                    baseScore = 6;
+                    break;
+                case "Cherry":
+                    baseScore = 5;
+                    break;
+                case "Bar":
+                    baseScore = 4;
+                    break;
+                case "King":
+                    baseScore = 3;
+                    break;
+                case "Queen":
+                    baseScore = 2;
+                    break;
+                case "Jack":
+                    baseScore = 1;
+                    break;
+                default:
+                    return 0;
+            }
+            //final score
+            if (threeSame) {
+                score = baseScore * 10;
+            } else if (twoSame) {
+                score = baseScore;
+            }
+
+            if (twoMoreWild) {
+                if ("Wild".equals(mostRepeat)) {
+                    score = 100;
+                } else {
+                    score *= 2;
+                }
+            }
+
+
+        }else {
+            return 0;
+        }
+        return score;
+    }
+
+}
---
+++ _6kyu/FruitMachine/README.md
@@ -0,0 +1,22 @@
+# Fruit Machine (6kyu)
+** Link:** [Solve this kata on Codewars](https://www.codewars.com/kata/590adadea658017d90000039)
+## Versions
+- `FruitMachine.java`: First working version.
+- ``: Refactoring version()
+
+## Examples
+```java
+        int[] elements =  {1, 2, 1, 2, 1, 2, 1, 2};
+		int maxOccurrences = 3;
+
+		System.out.println(Arrays.toString(deleteNth(elements, maxOccurrences)));//[1, 2, 1, 2, 1, 2]
+		
+		int[] elements1 =  {3, 3, 3, 3, 3};
+		int maxOccurrences1 = 10;
+
+		System.out.println(Arrays.toString(deleteNth(elements1, maxOccurrences1)));//[3, 3, 3, 3, 3]
+
+		int[] elements2 = {0, 0, 1, 0, 2, 2, 0, 3};
+		int maxOccurrences2 = 2;
+
+		System.out.println(Arrays.toString(deleteNth(elements2, maxOccurrences2)));//[0, 0, 1, 2, 2, 3]
