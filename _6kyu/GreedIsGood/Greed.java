4179a64e227067e29df6ca27e6bedf3bc71ebdf5
Bleedownz <g.6578@live.com>
2026-01-18 20:54:28 +0100

    kata next

---
+++ _6kyu/GreedIsGood/Greed.java
@@ -0,0 +1,47 @@
+package _6kyu.GreedIsGood;
+
+public class Greed {
+    public static void main(String[] args) {
+        int [] dice1 = {2,2,4,1,2};
+        int [] dice2 = {1,1,2,1,2};
+        int [] dice3 = {4,4,4,1,1};
+        System.out.println(greedy(dice1));
+        System.out.println(greedy(dice2));
+        System.out.println(greedy(dice3));
+    }
+    public static int greedy(int[] dice){
+        int score = 0;
+        int [] count = new int[6];
+        //Count the repeat number the same number
+        for (int number: dice){
+            if (number >= 1 && number <= 6){
+                count[number - 1]++;
+            }else{
+                System.out.println("Invalid number");
+            }
+        }
+        for (int i = 0; i < count.length; i++){
+            int sided = count[i];
+
+            if (sided >= 3){
+                //number 1 three times
+                if (i == 0) {
+                    score = score + 1000;
+                }else{
+                    //any number three times without the one
+                    score = score + (i + 1) * 100;
+                }
+                sided = sided - 3;
+            }
+            //Sum to the score the rest the 1s and 5s
+            if (i == 0){
+                score = score + (sided * 100);
+            }
+            if(i == 4){
+                score = score + sided * 50;
+            }
+
+        }
+        return score;
+    }
+}
---
+++ _6kyu/GreedIsGood/README.md
@@ -0,0 +1,14 @@
+# Greed is Good (6kyu)
+** Link:** [Solve this kata on Codewars](https://www.codewars.com/kata/5270d0d18625160ada0000e4/java)
+## Versions
+- `Greed.java`: First working version.
+- ``: Refactoring version()
+
+## Examples
+```java
+        int [] dice1 = {2,5,4,2,2};
+        int [] dice2 = {5,3,5,1,5};
+        int [] dice3 = {5,3,4,3,3};
+        System.out.println(greedy(dice1));//250
+        System.out.println(greedy(dice2));//600
+        System.out.println(greedy(dice3));//350
